7e6e7be87d9820f16aa0af209e53c8e349cda8fc
pedrowille <pedrowille9@gmail.com>
2021-11-24 16:25:25 -0300

    Atualizados exercicios

--- exercicio06/Program.cs
+++ exercicio06/Program.cs
@@ -8,37 +8,32 @@ namespace Exercicio06
         {
 Console.WriteLine("Programa exibe dias da semana:\nInforme o número correspondente ao dia:");
 int dia=Convert.ToInt32(Console.ReadLine());
-if (dia==1)
+switch (dia)
 {
+    case 1:
  Console.WriteLine("Domingo.");   
-}
-else if (dia==2)
-{
-    Console.WriteLine("Segunda-feira.");
-}
-else if (dia==3)
-{
+ break;
+    case 2:
+                                            Console.WriteLine("Segunda-feira.");
+break;    
+    case 3:
     Console.WriteLine("Terça-feira.");
-}
-        else if (dia==4)
-{
+    break;
+    case 4:
     Console.WriteLine("Quarta-feira.");
-}        
-else if (dia==5)
-        {
+            break;
+            case 5:
             Console.WriteLine("Quinta-feira.");
-        }
-else if (dia==6)
-        {
+            break;
+            case 6:
             Console.WriteLine("Sexta-feira.");
-        }
-else if (dia==7)
-        {
+            break;
+            case 7:
             Console.WriteLine("Sábado.");
-        }
-else
-{
-    Console.WriteLine("Valor inválido.");
+break;
+        default:
+        Console.WriteLine("Valor inválido.");
+break;
 }
         }
     }
