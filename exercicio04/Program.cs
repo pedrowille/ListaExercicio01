6d79208b6fbffb1c5e34fb05177764475313e87e
pedrowille <pedrowille9@gmail.com>
2021-11-24 13:33:15 -0300

    Exercício 05

---
+++ exercicio04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercicio02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+Console.WriteLine("Programa reajuste salarial:\nInforme o valor do salário:");
+string? textoSalario=Console.ReadLine();
+        double salario=Convert.ToDouble(textoSalario);
+double percentualAumento = 0;;
+double valorAumento = 0;
+double salarioReajustado = 0;
+if (salario <= 2800)
+{
+    percentualAumento=20.0;
+}
+else if (salario <=7000 && salario >2800)        
+        {
+    percentualAumento=15.0;
+}        
+else if (salario <= 15000 && salario > 7000)        
+{
+    percentualAumento=10.0;
+}
+else if (salario > 15000)
+{
+    percentualAumento=5.0;
+}
+valorAumento = (percentualAumento/100) * salario;
+salarioReajustado=salario+valorAumento;
+Console.WriteLine("Valor anterior do salário: " + salario + "\nPercentual de aumento: " + percentualAumento + "\nValor do aumento: " + valorAumento + "\nValor do salário reajustado: " + salarioReajustado);
+
+        }
+
+        }
+    }
